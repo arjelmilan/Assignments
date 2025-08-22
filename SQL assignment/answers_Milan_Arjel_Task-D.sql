007bbd1369e68ad3314c653bbc67348bf19e9758
arjelmilan <39267272+arjelmilan@users.noreply.github.com>
2025-08-22 09:49:36 +0545

    answers_Milan_Arjel_Task-D.sql

--- SQL assignment/answers_Milan_Arjel_Task-D.sql
+++ SQL assignment/answers_Milan_Arjel_Task-D.sql
@@ -1,3 +1,4 @@
+--1
 create or replace procedure sp_apply_category_discount(p_category text, p_percent numeric)
 language plpgsql
 as $$
@@ -13,6 +14,7 @@ $$;
 
 call sp_apply_category_discount('Electronics', 10);
 
+--2
 create or replace procedure sp_cancel_order(p_order_id int)
 language plpgsql
 as $$
@@ -28,13 +30,13 @@ begin
     raise notice 'Order % has been cancelled (if not already delivered).', p_order_id;
 end;
 $$;
-call sp_cancel_order(3);  -- cancels order 3 if not delivered
+call sp_cancel_order(3);
 
+--3
 create or replace procedure sp_reprice_stale_products(p_days int, p_increase numeric)
 language plpgsql
 as $$
 begin
-    -- Update products that have not been ordered in the last p_days
     update products p
     set unit_price = unit_price * (1 + p_increase/100.0)
     where p.active = true
@@ -45,10 +47,7 @@ begin
           where i.product_id = p.product_id
             and o.order_date >= current_date - (p_days || ' days')::interval
       );
-
-    raise notice 'Stale products repriced by % percent.', p_increase;
 end;
 $$;
 call sp_reprice_stale_products(30, 10);  
--- increases unit_price by 10% for products not ordered in the last 30 days
 
